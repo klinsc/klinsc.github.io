1155d082b3191d51fdd9311aa983879714ea4953
klinsc <klinsc.sea@live.com>
2020-11-30 10:40:48 +0700

    Revert "4"
    This reverts commit 13467aafdc4ff27533056751fd814c5096f05aa2.

---
+++ merge-script.js
@@ -0,0 +1,164 @@
+// If absolute URL from the remote server is provided, configure the CORS
+// header on that server.
+//
+
+
+var urls = [
+    "https://partner.shopeemobile.com/api/v1/logistics/download_shipping_document?token=ovpMYQQvL%2FKv5B8qy%2BbpCnLAO9Wn1wxGti6t63gZKVXV0GLLQD9xl9rM9unSX90JOEj3wone6%2F2PZYv7lTPUc4zykzcF1w0gbpVrAhpYSm3H3xCc3Ds0Es17TlBAEyFnFitKq64%2FirllC5tODdqw%2B7T1tYlTl0nLlLgxkaJzyPEpX7h42kcxVIQSKTjw5pG1CY00Zkv8XxXJpbd3BBHeJg80YgZ44p8OYdnqqg4dDyL5D8iDqOwMDWIeYAPlwNxkI99fpOMp2sK1SFZYUQVuasWMyJH2Lfqse9F1yr5ozE%2F9NFYEpxQiR5ast91NAifTXPNzXmXyBoaRss7aJKJZOw%3D%3D",
+    "https://partner.shopeemobile.com/api/v1/logistics/download_shipping_document?token=K8rsXmspP5YXKu5cPe6Vwr6mVB5QtnLaQQjGFMQeqqzQY7PjD5wv1XOQcjhQyF5HHezTtABJ5iY%2BruFkI9Ovd93FYKvohm28boAEAxFTYTN6dDkCAQhXQLqTkk2P%2BzcJuoEWWeOz%2Fdjlz3ytWO6OcysONqHilAxZZGcXLwdw8PR6n38mKNr2IC4vIt5YgKmPv5Ez%2Bsu0TEJHu2sreTveotqgJ6TJYjtHmD5w%2FdRpw6KOMqb78IZHGslcj53sCgfWgFYfIDwaMSAJ%2BMTVAtiJxQ6E3wevCSAqx0DB5Z20Zus%2FNRVnstnU%2B5y4c8dfSXOFEG9dym9Mt2fR2ntRyY5oOw%3D%3D",
+  ];
+  
+  // Disable workers to avoid yet another cross-origin issue (workers need
+  // the URL of the script to be loaded, and dynamically loading a cross-origin
+  // script does not work).
+  //
+  // pdfjsLib.disableWorker = true;
+  
+  // In cases when the pdf.worker.js is located at the different folder than the
+  // pdf.js's one, or the pdf.js is executed via eval(), the workerSrc property
+  // shall be specified.
+  //
+  // pdfjsLib.workerSrc = 'pdf.worker.js';
+  
+  /**
+   * @typedef {Object} PageInfo
+   * @property {number} documentIndex
+   * @property {number} pageNumber
+   */
+  
+  var pdfDocs = [],
+    /**
+     * @property {PageInfo}
+     */
+    current = {},
+    totalPageCount = 0,
+    pageNum = 1,
+    pageRendering = false,
+    pageNumPending = null,
+    scale = 0.8,
+    canvas = document.getElementById("the-canvas"),
+    ctx = canvas.getContext("2d");
+  
+  /**
+   * Get page info from document, resize canvas accordingly, and render page.
+   * @param num Page number.
+   */
+  function renderPage(num) {
+    pageRendering = true;
+    current = getPageInfo(num);
+    // Using promise to fetch the page
+    pdfDocs[current.documentIndex].getPage(current.pageNumber).then(function(page) {
+      var viewport = page.getViewport({ scale: scale });
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+  
+      // Render PDF page into canvas context
+      var renderContext = {
+        canvasContext: ctx,
+        viewport: viewport,
+      };
+      var renderTask = page.render(renderContext);
+  
+      // Wait for rendering to finish
+      renderTask.promise.then(function() {
+        pageRendering = false;
+        if (pageNumPending !== null) {
+          // New page rendering is pending
+          renderPage(pageNumPending);
+          pageNumPending = null;
+        }
+      });
+    });
+  
+    // Update page counters
+    document.getElementById("page_num").textContent = pageNum;
+  }
+  
+  /**
+   * If another page rendering in progress, waits until the rendering is
+   * finished. Otherwise, executes rendering immediately.
+   */
+  function queueRenderPage(num) {
+    if (pageRendering) {
+      pageNumPending = num;
+    } else {
+      renderPage(num);
+    }
+  }
+  
+  /**
+   * Displays previous page.
+   */
+  function onPrevPage() {
+    if (pageNum <= 1) {
+      return;
+    }
+    pageNum--;
+    queueRenderPage(pageNum);
+  }
+  
+  document.getElementById("prev").addEventListener("click", onPrevPage);
+  
+  /**
+   * Displays next page.
+   */
+  function onNextPage() {
+    if (pageNum >= totalPageCount && current.documentIndex + 1 === pdfDocs.length) {
+      return;
+    }
+  
+    pageNum++;
+    queueRenderPage(pageNum);
+  }
+  
+  document.getElementById("next").addEventListener("click", onNextPage);
+  
+  /**
+   * @returns PageNumber
+   */
+    function getPageInfo(num) {
+      let totalPageCount = 0;
+  
+      for (let i = 0; i < pdfDocs.length; i++) {
+        let currentCount = pdfDocs[i].numPages;
+        totalPageCount += currentCount;
+        if (num <= totalPageCount) {
+          return {
+            documentIndex: i,
+            pageNumber: (currentCount - (totalPageCount - num)),
+          };
+        }
+      }
+  
+      return false;
+    }
+  
+  function getTotalPageCount() {
+    var totalPageCount = 0;
+    for (var docIdx = 0; docIdx < pdfDocs.length; docIdx++) {
+      totalPageCount += pdfDocs[docIdx].numPages;
+    }
+    return totalPageCount;
+  }
+  
+  var loadedCount = 0;
+  
+  function load() {
+    // Load PDFs one after another
+    pdfjsLib.getDocument(urls[loadedCount])
+      .promise.then(function(pdfDoc_) {
+      console.log("loaded PDF " + loadedCount);
+      pdfDocs.push(pdfDoc_);
+      loadedCount++;
+      if (loadedCount !== urls.length) {
+        return load();
+      }
+  
+      console.log("Finished loading");
+      totalPageCount = getTotalPageCount();
+      document.getElementById("page_count").textContent = totalPageCount;
+  
+      // Initial/first page rendering
+      renderPage(pageNum);
+    });
+  }
